bbfb5943e704589f48ed92051897d994b6e9cff0
renjithspace <renjithrajeevvk@gmail.com>
2022-06-03 12:17:19 +0530

    Initial commit

---
+++ readme.md
@@ -0,0 +1,9 @@
+# Paser table
+Parse HTML table data from DOM to JSON
+
+### Usage
+```js
+import parseTable from 'parse-table'
+
+const jsonData = parserTable('tableCssSelectorHere')
+```
---
+++ src/lib.ts
@@ -0,0 +1,18 @@
+export default function parseTable (selector: string) {
+  const table = document.querySelector(selector)
+  if (table) {
+    const headers = [...table.querySelectorAll('th')]
+    const keys = headers.map(header => {
+        return header.innerText
+    })
+    const rows = [...table.querySelectorAll('tr')]
+    const data = rows.map(row => {
+        const cells = [...row.querySelectorAll('td')]
+        return cells.map((cell, index) => {
+            const key = keys[index]
+            return { [key]: cell.innerText }
+        })
+    })
+    return data.filter(data => data.length)
+  } else return []
+}
